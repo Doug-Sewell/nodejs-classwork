df897b0218f0e0a9abe5af217911866498f054db
Doug Sewell <DouglasASewell@gmail.com>
2020-03-04 21:06:49 -0500

    Learned JSON parsing and using the yargs package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,54 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
+//Customize yargs version:
+yargs.version('1.1.0');
 
-const msg = getNotes();
+//Create add command
+yargs.command({
+    command: 'add',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type:'string',
+        }
+    },
+    describe: 'Add a new note',
+    handler: (argv) => console.log(`Title: ${argv.title} \nBody: ${argv.body}`)
+});
 
-console.log(msg);
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => console.log('Removing the note.')
+});
+
+//Create list command
+yargs.command({
+    command:'list',
+    description:'Listing notes',
+    handler: () => console.log('Here\'s your notes!')
+});
+
+//Create read command
+yargs.command({
+    command:'read',
+    description:'Here\'s your note!',
+    handler:() => console.log('Here\'s your note!')
+})
+
+
+//add, remove, read, list
+
+
+
+yargs.parse();
+
+//console.log(yargs.argv);
