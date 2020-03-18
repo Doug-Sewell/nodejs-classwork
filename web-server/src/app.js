d3e7b24a396838922b7e3a3b115b7e026fd6505a
Doug Sewell <DouglasASewell@gmail.com>
2020-03-17 22:26:04 -0400

    Added new package for templating & set up views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,10 +3,32 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname,'../public');
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
 //What the server should do when the user visits
 //a certain url. (Send back HTML? JSON?)
+
+app.get('', (req,res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Doug'
+    });
+});
+
+app.get('/about',(req,res) => {
+    res.render('about', {
+        title:'About Me',
+        name: 'Doug'
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        prefferredCommunication: 'email'
+    });
+})
 
 app.get('/weather',(req,res) => {
     res.send({
